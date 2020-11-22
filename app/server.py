00ab200c73560f028a0e97f58f986aa4b6b73752
Tom Sako <38312729+tomassa@users.noreply.github.com>
2020-11-22 21:35:46 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1H5NOs9b9r1-RHvyz52UV-iHW9xs-HPjW'
+export_file_name = 'lrn_f2_nn_lesni.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Lenka_test', 'Lukas_test', 'Sabinaneuronka','Sebastian_foto']
 path = Path(__file__).parent
 
 app = Starlette()
